a76fd71cbcb4b5ebd20168f364b71995e9db5f53
shoobah <shoobah@gmail.com>
2013-11-01 00:03:46 +0100

    Added som markup to calculate etc.

--- routes/calculate.js
+++ routes/calculate.js
@@ -1,4 +1,5 @@
 exports.docalc = function(req, res){
-    var myAnswer=7;
-    res.render('calculate', { title: 'Calculate color diff', answer:myAnswer });
+    var picker1Hex=req.body.picker1;
+    var picker2Hex=req.body.picker2;
+    res.render('calculate', { title: 'Calculate color diff', answer1: picker1Hex, answer2: picker2Hex });
 };
